bd9df348b51123320a5da0e9a15ba7ced691f2d1
Xander Steenbrugge <xandersteenbruggex@gmail.com>
2025-01-31 17:49:07 +0100

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/guides/creation">
+            to="/docs/guides/agents">
             Get Started
           </Link>
         </div>
